173e51d0ccf6620a1acea2078e636178405ea149
miguel javela <mjavela@compuamerica.com.ve>
2022-05-07 16:21:52 -0500

    clase 4 - endpoinst y query parameters

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-const URL = "https://api.thecatapi.com/v1/images/search"
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"
 
-fetch(URL)
+fetch(API_URL)
     .then(res => res.json())
     .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        const img3 = document.getElementById('img3');
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+        img3.src = data[2].url;
     })
 
 async function cambiarImagen (){
-    const URL = "https://api.thecatapi.com/v1/images/search"
-    const response = await fetch (URL);
+    const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"
+    const response = await fetch (API_URL);
     data = await response.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    console.log(data)
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
