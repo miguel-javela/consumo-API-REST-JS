02d8ba2a380fb5a0e3d92fad64eb1c35c31d6c7b
miguel javela <mjavela@compuamerica.com.ve>
2022-05-07 19:43:31 -0500

    clase 9 - GET: leyendo michis favoritos

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3"
+// fetch(API_URL)
+//     .then(res => res.json())
+//     .then(data => {
+//         const img1 = document.getElementById('img1');
+//         const img2 = document.getElementById('img2');
+//         const img3 = document.getElementById('img3');
+//         img1.src = data[0].url;
+//         img2.src = data[1].url;
+//         img3.src = data[2].url;
+//     })
 
-fetch(API_URL)
-    .then(res => res.json())
-    .then(data => {
+const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=d2945296-d352-499f-90c3-710dfe04b855"
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=d2945296-d352-499f-90c3-710dfe04b855"
+const spanError = document.getElementById("error");
+
+async function loadRandomMichis (){
+    const res = await fetch (API_URL_RANDOM);
+    data = await res.json();
+
+    if(res.status !== 200){
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
-        const img3 = document.getElementById('img3');
         img1.src = data[0].url;
         img2.src = data[1].url;
-        img3.src = data[2].url;
-    })
-
-async function cambiarImagen (){
-    const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=d2945296-d352-499f-90c3-710dfe04b855"
-    const response = await fetch (API_URL);
-    data = await response.json();
-
-    console.log(data)
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    }
 }
+
+
+async function loadFavoritesMichis (){
+    const res = await fetch (API_URL_FAVORITES);
+    data = await res.json();
+
+    console.log("favoritosssss")
+    console.log(data)
+
+    if(res.status !== 200){
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    }
+
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
