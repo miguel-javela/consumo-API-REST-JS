f7f696cab66913d8aff800324eea78f7570845c6
miguel javela <mjavela@compuamerica.com.ve>
2022-05-08 23:04:53 -0500

    clase 11 - consultas a la API para escribir HTML dinamico

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 //     })
 
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=d2945296-d352-499f-90c3-710dfe04b855"
-const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=d2945296-d352-499f-90c3-710dfe04b855"
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?api_key=d2945296-d352-499f-90c3-710dfe04b855"
 const spanError = document.getElementById("error");
 
 async function loadRandomMichis (){
@@ -22,8 +22,13 @@ async function loadRandomMichis (){
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavouriteMichi(data[0].id);
+        btn2.onclick = () => saveFavouriteMichi(data[1].id);
     }
 }
 
@@ -32,23 +37,37 @@ async function loadFavouritesMichis (){
     const res = await fetch (API_URL_FAVORITES);
     const data = await res.json();
 
-    console.log("favoritosssss")
-    console.log(data)
-
     if(res.status !== 200){
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    } else {
+        console.log("favoirtos",data)
+        data.forEach(michi => {
+            const section = document.getElementById('favoriteMichis');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('sacar al michi de favoritos');
+
+            img.src = michi.image.url;
+            img.width = 150;
+            btn.appendChild(btnText);
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        })
     }
 
 }
 
-async function saveFavouriteMichis (){
+async function saveFavouriteMichi (id){
+    console.log("id",id)
     const res = await fetch(API_URL_FAVORITES,{
         method: 'POST',
         headers:{
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: 'dje'
+            image_id: id
         })
     })
 
@@ -57,10 +76,8 @@ async function saveFavouriteMichis (){
     if(res.status !== 200){
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
     }
-
-
-    console.log(res)
 }
+  
 
 loadRandomMichis();
 loadFavouritesMichis();
