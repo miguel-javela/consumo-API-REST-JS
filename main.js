253beeba9727f01afb06ccd9117e15ebc4c98956
miguel javela <mjavela@compuamerica.com.ve>
2022-05-09 22:14:34 -0500

    clase 16 - formData: publicando imagenes de michis

--- main.js
+++ main.js
@@ -11,6 +11,8 @@
 
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2"
 const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites"
+const API_URL_UPLOAD = "https://api.thecatapi.com/v1/images/upload"
+
 const API_URL_FAVORITES_DELETE = (id) => {
     return `https://api.thecatapi.com/v1/favourites/${id}`;
 }
@@ -112,7 +114,36 @@ async function deleteFavouriteMichi(id){
         loadFavouritesMichis();
     }
 }
+
+async function uploadMichiPhoto() {
+    const form = document.getElementById('uploadingForm')
+    const formData = new FormData(form);
   
+    console.log(formData.get('file'))
+  
+    const res = await fetch(API_URL_UPLOAD, {
+      method: 'POST',
+      headers: {
+        // 'Content-Type': 'multipart/form-data',
+        'X-API-KEY': 'c08d415f-dea7-4a38-bb28-7b2188202e46',
+      },
+      body: formData,
+    })
+    const data = await res.json();
+  
+    if (res.status !== 201) {
+      spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+      console.log({data})
+    } else {
+      console.log('Foto de michi subida :)')
+      console.log({data})
+      console.log(data.url)
+      saveFavouriteMichi(data.id);
+    }
+  }
+
+
+
 
 loadRandomMichis();
 loadFavouritesMichis();
